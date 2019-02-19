45b30c55710c90343f64b17df324a4ab4c955b7f
Kevin Barabash <kevinb7@gmail.com>
2019-02-19 18:49:50 -0500

    Modernize codebase (#10)
    - switch to rollup
    - switch to jest
    - use @babel/env
    - allow => and ... in build output

--- src/index.js
+++ src/index.js
@@ -33,140 +33,103 @@ const _curryN = (n, received, fn) =>
     };
 
 // curryN :: Number -> (* -> a) -> (* -> a)
-const curryN = (n, fn) => arity(n, _curryN(n, [], fn));
+export const curryN = (n, fn) => arity(n, _curryN(n, [], fn));
 
 // curry :: (* -> a) -> (* -> a)
-const curry = (fn) => curryN(fn.length, fn);
+export const curry = (fn) => curryN(fn.length, fn);
 
 // reduce :: ((a, b) -> a) -> a -> [b] -> a
-const reduce = curry((f, init, arr) => arr.reduce(f, init));
+export const reduce = curry((f, init, arr) => arr.reduce(f, init));
 
 // reduceRight :: ((a, b) -> a) -> a -> [b] -> a
-const reduceRight = curry((f, init, arr) => arr.reduceRight(f, init));
+export const reduceRight = curry((f, init, arr) => arr.reduceRight(f, init));
 
 // compose :: Function f => [f] -> (a -> b)
-const compose = (...farr) => x => farr.reduceRight((v, f) => f(v), x);
+export const compose = (...farr) => x => farr.reduceRight((v, f) => f(v), x);
 
 // pipe :: Function f => [f] -> (a -> b)
-const pipe = (...farr) => x => farr.reduce((v, f) => f(v), x);
+export const pipe = (...farr) => x => farr.reduce((v, f) => f(v), x);
 
 // map :: (a -> b) -> [a] -> [b]
-const map = curry((f, arr) => arr.map(f));
+export const map = curry((f, arr) => arr.map(f));
 
 // filter :: (a -> Boolean) -> [a] -> [a]
-const filter = curry((f, arr) => arr.filter(f));
+export const filter = curry((f, arr) => arr.filter(f));
 
 // find :: (a -> Boolean) -> [a] -> a
-const find = curry((f, arr) => arr.find(f));
+export const find = curry((f, arr) => arr.find(f));
 
 // findIndex :: (a -> Boolean) -> [a] -> Number
-const findIndex = curry((f, arr) => arr.findIndex(f));
+export const findIndex = curry((f, arr) => arr.findIndex(f));
 
 // flatten :: [a] -> [b]
-const flatten = reduce(
+export const flatten = reduce(
     (res, v) => res.concat(Array.isArray(v) ? flatten(v) : [v]), []);
 
 // flatMap :: (a -> [b]) -> [a] -> [b]
-const flatMap = curry((f, arr) => compose(flatten, map(f))(arr));
+export const flatMap = curry((f, arr) => compose(flatten, map(f))(arr));
 
 // all :: (a -> Boolean) -> [a] -> Boolean
-const all = curry((f, arr) => reduce((res, v) => res && f(v), true, arr));
+export const all = curry((f, arr) => reduce((res, v) => res && f(v), true, arr));
 
 // none :: (a -> Boolean) -> [a] -> Boolean
-const none = curry((f, arr) => !reduce((res, v) => res || f(v), false, arr));
+export const none = curry((f, arr) => !reduce((res, v) => res || f(v), false, arr));
 
 // some :: (a -> Boolean) -> [a] -> Boolean
-const some = curry((f, arr) => reduce((res, v) => res || f(v), false, arr));
+export const some = curry((f, arr) => reduce((res, v) => res || f(v), false, arr));
 
 // split :: (String | RegExp) -> String -> [String]
-const split = curry((splitOn, str) => str.split(splitOn));
+export const split = curry((splitOn, str) => str.split(splitOn));
 
 // replace :: (String | RegExp) -> String -> String
-const replace = curry((s, r, str) => str.replace(s, r));
+export const replace = curry((s, r, str) => str.replace(s, r));
 
 // remove :: String a => a -> a -> a
-const remove = curry((s, str) => replace(s, '')(str));
+export const remove = curry((s, str) => replace(s, '')(str));
 
 // eq :: a -> b -> Boolean
 // NOTE: shallow equals
-const eq = curry((a, b) => a === b);
+export const eq = curry((a, b) => a === b);
 
 // neq :: a -> b -> Boolean
 // NOTE: shallow not equals
-const neq = curry((a, b) => a !== b);
+export const neq = curry((a, b) => a !== b);
 
 // prop :: s -> {s: a} -> a | Undefined
-const prop = curry((k, obj) => obj[k]);
+export const prop = curry((k, obj) => obj[k]);
 
 // propEq :: String -> a -> Object -> Boolean
-const propEq = curry((k, v, obj) => compose(eq(v), prop(k))(obj));
+export const propEq = curry((k, v, obj) => compose(eq(v), prop(k))(obj));
 
 // last :: [a] -> a
-const last = (arr) => arr[arr.length - 1];
+export const last = (arr) => arr[arr.length - 1];
 
 // first :: [a] -> a
-const first = (arr) => arr[0];
+export const first = (arr) => arr[0];
 
 // id :: a -> a
-const id = (a) => a;
+export const id = (a) => a;
 
 // length :: [a] -> Number
-const length = (arr) => arr.length;
+export const length = (arr) => arr.length;
 
 // fromEntries :: [[k,v]] → {k: v}
-const fromEntries =
+export const fromEntries =
     (entries) => reduce((obj, entry) => (obj[entry[0]] = entry[1], obj), {}, entries);
 
 // sum :: [Number] -> Number
-const sum = (arr) => reduce((res, v) => res + v, 0, arr);
+export const sum = (arr) => reduce((res, v) => res + v, 0, arr);
 
 // tap :: fn -> a -> a
-const tap = curry((fn, a) => (fn(a), a));
+export const tap = curry((fn, a) => (fn(a), a));
 
 // log :: a -> void
-const log = (a) => console.log(a);
+export const log = (a) => console.log(a);
 
 // join :: String -> [a] -> String
-const join = curry((s, arr) => arr.join(s));
+export const join = curry((s, arr) => arr.join(s));
 
 // uniq :: [a] -> [a]
 // NOTE: doesn't handle structures
-const uniq = (arr) => reduce(
+export const uniq = (arr) => reduce(
     (res, v) => res.indexOf(v) !== -1 ? res : (res.push(v), res), [], arr);
-
-module.exports = {
-    all,
-    some,
-    compose,
-    curry,
-    curryN,
-    eq,
-    filter,
-    find,
-    findIndex,
-    first,
-    flatMap,
-    flatten,
-    fromEntries,
-    fromPairs: fromEntries,
-    id,
-    identity: id,
-    join,
-    last,
-    length,
-    log,
-    map,
-    neq,
-    none,
-    pipe,
-    prop,
-    propEq,
-    reduce,
-    reduceRight,
-    remove,
-    replace,
-    split,
-    tap,
-    sum,
-    uniq,
-};
